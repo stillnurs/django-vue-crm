dbc9c079c54b4bc5db6ef14101715ef359f8f2eb
stillnurs <67423522+stillnurs@users.noreply.github.com>
2022-07-19 21:37:28 +0400

    added sign-up, login, logout functionalities

--- crm_vue/src/views/Login.vue
+++ crm_vue/src/views/Login.vue
@@ -3,19 +3,34 @@
     <div class="columns">
       <div class="column is-4 is-offset-4">
         <h1 class="title">Login</h1>
-        <form>
+
+        <form @submit.prevent="submitForm">
           <div class="field">
             <label>Email</label>
             <div class="control">
-              <input type="email" name="email" class="input" />
+              <input
+                type="email"
+                name="email"
+                class="input"
+                v-model="username"
+              />
             </div>
           </div>
 
           <div class="field">
             <label>Password</label>
             <div class="control">
-              <input type="password" name="password1" class="input" />
+              <input
+                type="password"
+                name="password"
+                class="input"
+                v-model="password"
+              />
             </div>
+          </div>
+
+          <div class="notification is-danger" v-if="errors.length">
+            <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
           </div>
 
           <div class="field">
@@ -30,7 +45,50 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Login",
+  data() {
+    return {
+      username: "",
+      password: "",
+      errors: [],
+    };
+  },
+  methods: {
+    submitForm() {
+      axios.defaults.headers.common["Authorization"] = "";
+      localStorage.removeItem("token");
+
+      const formData = {
+        username: this.username,
+        password: this.password,
+      };
+
+      axios
+        .post("/api/v1/token/login/", formData)
+        .then((response) => {
+          const token = response.data.auth_token;
+
+          this.$store.commit("setToken", token);
+
+          axios.defaults.headers.common["Authorization"] = "Token " + token;
+
+          localStorage.setItem("token", token);
+
+          this.$router.push("/dashboard/my-account");
+        })
+        .catch((error) => {
+          if (error.response) {
+            for (const property in error.response.data) {
+              this.errors.push(`${property}:${error.response.data[property]}`);
+            }
+          } else if (error.message) {
+            this.errors.push("Something went wrong. Please try again!");
+          }
+        });
+    },
+  },
 };
 </script>
